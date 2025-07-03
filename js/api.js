daf441024d335f2ee83c0c56af0c6c3d58205f0f
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-07-03 20:48:59 -0300

    Colocando os pensamentos com o backend e criando novos

--- js/api.js
+++ js/api.js
@@ -3,13 +3,32 @@
 const api = {
     async buscarPensamentos() {
         try {
-            const resposta = await fetch('http://localhost:3000/pensamentos');
+            const resposta = await fetch('http://localhost:3000/pensamentos');  // por padrão, o fetch usa o método GET
             return await resposta.json();
+
         } catch {
             alert('Erro ao buscar pensamentos!');
             throw error;
         };
-    }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const resposta = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+
+            return await resposta.json();
+
+        } catch {
+            alert('Erro ao buscar pensamentos!');
+            throw error;
+        };
+    },
 };
 
 export default api;
--- js/ui.js
+++ js/ui.js
@@ -7,20 +7,50 @@ const ui = {
 
         try {
             const pensamentos = await api.buscarPensamentos();
-            pensamentos.forEach(pensamento => {
-                listaDePensamentos.innerHTML += `
-                    <li class="li-pensamento" data-id="${pensamento.id}">
-                        <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-                        <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-                        <div class="pensamento-autoria">${pensamento.autoria}</div>
-                    </li>
-                `
-            });
+            // pensamentos.forEach(pensamento => {
+            //     listaDePensamentos.innerHTML += `
+            //         <li class="li-pensamento" data-id="${pensamento.id}">
+            //             <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
+            //             <div class="pensamento-conteudo">${pensamento.conteudo}</div>
+            //             <div class="pensamento-autoria">${pensamento.autoria}</div>
+            //         </li>
+            //     `
+            // });
+            pensamentos.forEach(ui.adicionarPensamentoNaLista);
         }
-        catch (error) {
+        catch {
             alert('Erro ao renderizar os pensamentos');
             throw error;
-        }
+        };
+    },
+
+    limparFormulario() {
+        document.getElementById('pensamento-form').reset();
+    },
+
+    adicionarPensamentoNaLista(pensamento) {
+        const listaDePensamentos = document.getElementById('lista-pensamentos');
+        const liPensamento = document.createElement('li');
+        liPensamento.setAttribute("data-id", pensamento.id);
+        liPensamento.classList.add("li-pensamento");
+
+        const iconeAspas = document.createElement('img');
+        iconeAspas.src = "assets/imagens/aspas-azuis.png";
+        iconeAspas.alt = "Aspas azuis";
+        iconeAspas.classList.add("icone-aspas");
+
+        const pensamentoConteudo = document.createElement('div');
+        pensamentoConteudo.textContent = pensamento.conteudo;
+        pensamentoConteudo.classList.add("pensamento-conteudo");
+
+        const pensamentoAutoria = document.createElement('div');
+        pensamentoAutoria.textContent = pensamento.autoria;
+        pensamentoAutoria.classList.add("pensamento-autoria");
+
+        liPensamento.appendChild(iconeAspas);
+        liPensamento.appendChild(pensamentoConteudo);
+        liPensamento.appendChild(pensamentoAutoria);
+        listaDePensamentos.appendChild(liPensamento);
     }
 };
 
